4e925dbc66a756a6506014918c27b220dc9ec3f1
baoPQ258 <ibaophan264@gmail.com>
2026-07-02 14:30:22 +0700

    first commit

---
+++ src/api/khoa-hoc/routes/khoa-hoc.ts
@@ -0,0 +1,7 @@
+/**
+ * khoa-hoc router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::khoa-hoc.khoa-hoc');
---
+++ src/api/khoa-hoc/services/khoa-hoc.ts
@@ -0,0 +1,7 @@
+/**
+ * khoa-hoc service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::khoa-hoc.khoa-hoc');
